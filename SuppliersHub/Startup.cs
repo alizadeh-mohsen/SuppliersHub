771a2fd483624954d3cf9da1b0b70b54ac1aa06b
Mohsen <kelarens@gmail.com>
2020-07-08 17:52:57 +0430

    Add project files.

---
+++ SuppliersHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuppliersHub.Startup))]
+namespace SuppliersHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
